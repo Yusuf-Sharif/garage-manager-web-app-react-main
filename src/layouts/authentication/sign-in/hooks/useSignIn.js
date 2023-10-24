102c39b14cf0b0a40e8ab1ecc157f26d5b85e014
Yusuf-Sharif <93823265+Yusuf-Sharif@users.noreply.github.com>
2023-10-24 16:27:14 +0100

    Refactoring: - Deleted console logs and uneeded commented code

--- src/layouts/authentication/sign-in/hooks/useSignIn.js
+++ src/layouts/authentication/sign-in/hooks/useSignIn.js
@@ -1,32 +1,17 @@
-import { auth } from "../../../../config/firebase.js";
 import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../../../config/firebase.js";
 
 function useSignIn() {
     const signIn = async (email, password) => {
         try {
           await signInWithEmailAndPassword(auth, email, password)
-          console.log("Signed In!")
         }
         catch(error) {
-          console.log("There was an error signing in");
-          console.log(JSON.stringify(error));
           if (error.code === "auth/user-not-found" || error.code === "auth/wrong-password" || error.code === "auth/user-disabled" || error.code === "auth/invalid-email") {
             const errorMsg = "Incorrect email or password";
             return errorMsg;
           }
-        }
-
-        // auth.onAuthStateChanged((user) => {
-        //   if (user) {
-        //     // User is signed in.
-        //     console.log('User is signed in:', user);
-        //   } else {
-        //     // No user is signed in.
-        //     console.log('User is not signed in');
-        //     console.log(user)
-        //   }
-        // });
-        
+        }       
     }
 
     return { signIn }
